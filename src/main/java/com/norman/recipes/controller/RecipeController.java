21e6d69856610adfe150a0c2ca7619f733fe435f
Mbouende Norman <mbouendenorman@gmail.com>
2021-08-09 18:18:21 +0200

    update cors to spring security

--- src/main/java/com/norman/recipes/controller/RecipeController.java
+++ src/main/java/com/norman/recipes/controller/RecipeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/recipe")
 @RequiredArgsConstructor
-@CrossOrigin(origins = {"http://localhost:8080", "https://apirecipe.normanmbouende.com/"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://recipe.normanmbouende.com/"})
 public class RecipeController {
     private final RecipeService recipeService;
 
